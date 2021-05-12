7cbf40fee9e21e21fc1c2517bd7bc23cc8d5b038
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2021-05-13 01:27:12 +0530

    Create richest customer wealth.py
    In a nested list add all the elements in each list in the nested list and find max and return maximum.

---
+++ richest customer wealth.py
@@ -0,0 +1,18 @@
+# Input: accounts = [[1,5],[7,3],[3,5]]
+# Output: 10
+# Explanation: 
+# 1st customer has wealth = 6
+# 2nd customer has wealth = 10 
+# 3rd customer has wealth = 8
+# The 2nd customer is the richest with a wealth of 10.
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return_list = []
+        for i in range(len(accounts):
+            result = return_list.append(sum(accounts[i]))
+            maximum = max(return_list)
+                
+        return maximum
+                
+                
